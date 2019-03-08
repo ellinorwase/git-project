f2f35cc38412346d46f3356dc00f6ad910417d3e
Ellinore <ellinor.wase@gmail.com>
2019-03-08 12:33:30 +0100

    drop-down on commands

--- searchbar.js
+++ searchbar.js
@@ -3,19 +3,20 @@ function searchFunction() {
 let inputSearch = document.getElementById('inputSearch').value;
 
 
-let arr = [document.getElementById('git'), document.getElementById('github'), document.getElementById('kommandon'),
+let arr = [document.getElementById('git'), document.getElementById('github'), document.getElementById('kommandon'), document.getElementById('commit-number'),
 document.getElementById('commit'), document.getElementById('status'), document.getElementById('pull'),
 document.getElementById('push'), document.getElementById('branch'), document.getElementById('add'),
 document.getElementById('log'), document.getElementById('checkout'), document.getElementById('work-with-git'),
-document.getElementById('branches'), document.getElementById("merge"), document.getElementById("remove"), document.getElementById("origin"), document.getElementById('checkout'), document.getElementById('init')];
+document.getElementById('branches'), document.getElementById("merge"), document.getElementById("remove"), document.getElementById("origin"), document.getElementById('checkout'), document.getElementById('init'), document.getElementById('diff')];
 
 for (let i = 0; i < arr.length; i++) {
 
 
     if (arr[i] && inputSearch == arr[i].id) {
         let link = arr[i];
-        console.log(`link`, link);
-        document.getElementById('searchBtn').href = link.href;
+        console.error(`link`, link.id);
+        document.getElementById('searchBtn').href = '#' + link.id;
+        // `#${link.id}`
         }
     }
 }
