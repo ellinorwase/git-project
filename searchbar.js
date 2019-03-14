ef213507d1a47170f20b9926e1fb86603f95983b
Ellinore <ellinor.wase@gmail.com>
2019-03-14 16:01:44 +0100

    final touch

--- searchbar.js
+++ searchbar.js
@@ -10,10 +10,10 @@ document.getElementById('push'), document.getElementById('branch'), document.get
 document.getElementById('log'), document.getElementById('checkout'), document.getElementById('work-with-git'),
 document.getElementById('branches'), document.getElementById("merge"), document.getElementById("remove"), document.getElementById("origin"), document.getElementById('checkout'), document.getElementById('init'), document.getElementById('diff'), document.getElementById('touch'), document.getElementById('fetch'), document.getElementById('clone'), document.getElementById('url')];
 
-//Lopas igenom arrayen och ser om det blir en match.
+//Loopar igenom arrayen och ser om det blir nån träff.
 for (let i = 0; i < array.length; i++) {
 
-//Om det blir en match länkas den via href attributet
+//Isåfall länkas den via href attributet
     if (array[i] && inputSearch == array[i].id) {
         let link = array[i];
         console.error(`link`, link.id);
